bfeef33d7e10ed3d3e9b0d2c64f66ccfac728a35
Rebecca Durst <rebeccadurst@gmail.com>
2022-11-03 08:09:58 -0500

    working on adding GIFs

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
 
 void main() {
   runApp(const HorizonsApp());
@@ -72,14 +71,13 @@ class WeeklyForecastList extends StatelessWidget {
 // --------------------------------------------
 // Below this line are helper classes and data.
 
-const String baseAssetURL =
-    'https://dartpad-workshops-io2021.web.app/getting_started_with_slivers/';
-const String headerImage = '${baseAssetURL}assets/header.jpeg';
+const String baseAssetURL = 'https://i.giphy.com/media/';
+const String headerImage = '${baseAssetURL}65u/200.gif';
 
 const Map<int, DailyForecast> _kDummyData = {
   0: DailyForecast(
     id: 0,
-    imageId: '${baseAssetURL}assets/day_0.jpeg',
+    imageId: '${baseAssetURL}ySu/200.gif',
     highTemp: 73,
     lowTemp: 52,
     description:
@@ -87,42 +85,42 @@ const Map<int, DailyForecast> _kDummyData = {
   ),
   1: DailyForecast(
     id: 1,
-    imageId: '${baseAssetURL}assets/day_1.jpeg',
+    imageId: '${baseAssetURL}A48l/200.gif',
     highTemp: 70,
     lowTemp: 50,
     description: 'Partly sunny.',
   ),
   2: DailyForecast(
     id: 2,
-    imageId: '${baseAssetURL}assets/day_2.jpeg',
+    imageId: '${baseAssetURL}TH2O/200.gif',
     highTemp: 71,
     lowTemp: 55,
     description: 'Party cloudy.',
   ),
   3: DailyForecast(
     id: 3,
-    imageId: '${baseAssetURL}assets/day_3.jpeg',
+    imageId: '${baseAssetURL}7Qpb/200.gif',
     highTemp: 74,
     lowTemp: 60,
     description: 'Thunderstorms in the evening.',
   ),
   4: DailyForecast(
     id: 4,
-    imageId: '${baseAssetURL}assets/day_4.jpeg',
+    imageId: '${baseAssetURL}1kve/200.gif',
     highTemp: 67,
     lowTemp: 60,
     description: 'Severe thunderstorm warning.',
   ),
   5: DailyForecast(
     id: 5,
-    imageId: '${baseAssetURL}assets/day_5.jpeg',
+    imageId: '${baseAssetURL}7sdC/200.gif',
     highTemp: 73,
     lowTemp: 57,
     description: 'Cloudy with showers in the morning.',
   ),
   6: DailyForecast(
     id: 6,
-    imageId: '${baseAssetURL}assets/day_6.jpeg',
+    imageId: '${baseAssetURL}Iqt/200.gif',
     highTemp: 75,
     lowTemp: 58,
     description: 'Sun throughout the day.',
